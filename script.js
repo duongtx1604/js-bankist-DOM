14ef6ff3ea69c7e0db4af013b14e970d6d745cfe
Duong Truong <91935964+duongtx1604@users.noreply.github.com>
2023-11-09 11:18:15 +0900

    add page navigation smooth scroll

--- script.js
+++ script.js
@@ -3,10 +3,16 @@
 ///////////////////////////////////////
 // Modal window
 
+const header = document.querySelector('.header');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const section2 = document.querySelector('#section--2');
+const section3 = document.querySelector('#section--3');
+const section4 = document.querySelector('#section--4');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -18,9 +24,7 @@ const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
-
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -30,3 +34,90 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// Button Smooth scroll
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+
+  // console.log('Current scroll (X/Y)', window.pageXOffset, pageYOffset);
+
+  // console.log(
+  //   'heigh/width viewport',
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth
+  // );
+
+  //Scrolling
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//-----------
+// Page Navigation
+
+// Method 1 (forEach)
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+// Method 2
+// Không forEach làm chậm tiến trình
+// Dùng listener lên cha của nó
+// Dùng e.targer trỏ đến phần tử được click
+//1. Add event listener to common parent element
+//2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+// const h1 = document.querySelector('h1');
+
+// const alertH1 = function (e) {
+//   alert('addEventListener');
+
+//   h1.removeEventListener('mouseenter', alertH1);
+// };
+
+// h1.addEventListener('mouseenter', alertH1);
+
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter');
+// };
+// //creating and inserting elements
+// const messsage = document.createElement('div');
+// messsage.classList.add('cookie-message');
+
+// //add content
+// messsage.innerHTML = `We use cookied for improved functionality and analytics. <button class ="btn btn--close-cookie">Got it!</button>`;
+
+// //add element to html
+// header.append(messsage);
+
+// //delete elements
+// document
+//   .querySelector('.btn--close-cookie')
+//   .addEventListener('click', function () {
+//     messsage.remove();
+//   });
+
+// //Styles
+// messsage.style.backgroundColor = '#37383d';
+// messsage.style.width = '120%';
+// messsage.style.height =
+//   Number.parseFloat(getComputedStyle(messsage).height, 10) + 30 + 'px';
